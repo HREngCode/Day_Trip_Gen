19493245989da97a0002f7d8c113fce7d50801ec
chad <ccrsailors@gmail.com>
2022-06-05 17:34:30 -0500

    Added output message confirming user's selections.

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 #Make 3 commits with descriptive messages
-
 import random
 
 #places to visit
@@ -11,47 +10,50 @@ def select_destination (destination):
 
 place_selected = select_destination (random.choice(destinations))
 
+#Opening to select destination
 print ("Welcome to the Day Trip Generator. We hope to make your travel planning as easy as possible. Let's start with a location. How about " + place_selected + "? Does that sound like a good idea? type yes or no"  )
 place_answer = input()
+# place_answer = check_answer(place_answer)
+while place_answer != "yes" and place_answer != "no":
+    print("You must enter yes or no")
+    place_answer = input()
 
 while place_answer == "no":
     place_selected = select_destination (random.choice(destinations))
     print("We want to make sure you are satisfied. Let's try another place.")
     print("How about " + place_selected + " ?")
     place_answer = input()
-
-    if place_answer != "yes" and place_answer != "no":
+    while place_answer != "yes" and place_answer != "no":
         print("You must enter yes or no")
         place_answer = input()
     
-    else:
-        destination = place_selected
-
 #places to eat
 restaurants = ['Carrabas', 'On The Border', 'Texas Roadhouse', 'Joes Crab Shack', 'PF Changs'] 
 
-#randomly select restaurant
-def select_restaurant (restaurant):
-    return restaurant
+# randomly select restaurant
+if place_answer == "yes":
+    def select_restaurant (restaurant):
+        return restaurant
 
-restaurant_selected = select_restaurant (random.choice(restaurants))
+    restaurant_selected = select_restaurant (random.choice(restaurants))
 
+
+#validate selected restaurant
 if place_answer == "yes":
     print("Now that you have a destination, you can pick a restaurant. How does " + restaurant_selected + " sound?")
     rest_answer = input("Please type yes or no." )
-
-while rest_answer == "no":
-    restaurant_selected = select_restaurant (random.choice(restaurants))
-    print("Not what you're in the mood for. Let's try something else.")
-    print("How about " + restaurant_selected + " ?")
-    rest_answer = input()
-
-    if rest_answer != "yes" and rest_answer != "no":
+    while rest_answer != "yes" and rest_answer != "no":
         print("You must enter yes or no")
         rest_answer = input()
-    
-    else:
-        restaurant = restaurant_selected
+
+    while rest_answer == "no":
+        restaurant_selected = select_restaurant (random.choice(restaurants))
+        print("Not what you're in the mood for. Let's try something else.")
+        print("How about " + restaurant_selected + " ?")
+        rest_answer = input()
+        while rest_answer != "yes" and rest_answer != "no":
+            print("You must enter yes or no")
+            rest_answer = input()
 
 #methods of transportation
 trans_methods = ['Boat', 'Train', 'Car', 'Plane'] 
@@ -62,22 +64,22 @@ def select_transportation (transportation):
 
 transportation_selected = select_transportation (random.choice(trans_methods))
 
+#validate selected transportation
 if rest_answer == "yes":
     print("How do you prefer to get to your destination? Would a " + transportation_selected + " work?")
     trans_answer = input("Please type yes or no." )
-
-while trans_answer == "no":
-    transportation_selected = select_restaurant (random.choice(trans_methods))
-    print("Not what you had in mind?")
-    print("How about a " + transportation_selected + " ?")
-    trans_answer = input()
-
-    if trans_answer != "yes" and trans_answer != "no":
+    while trans_answer != "yes" and trans_answer != "no":
         print("You must enter yes or no")
         trans_answer = input()
-    
-    else:
-        transportation = transportation_selected
+
+    while trans_answer == "no":
+        transportation_selected = select_transportation (random.choice(trans_methods))
+        print("Not what you had in mind?")
+        print("How about a " + transportation_selected + " ?")
+        trans_answer = input()
+        while trans_answer != "yes" and trans_answer != "no":
+            print("You must enter yes or no")
+            trans_answer = input()
 
 #forms of entertainment
 ent_forms = ['Dance Club', 'Comedy Bar', 'Playhouse Theater', 'Cinema', 'Escape Room'] 
@@ -89,24 +91,45 @@ def select_entertainment (entertainment):
 entertainment_selected = select_entertainment (random.choice(ent_forms))
 print(entertainment_selected)
 
+#validate selected entertainment
 if trans_answer == "yes":
     print("What type of entertainment are you looking for? Would a " + entertainment_selected + " be on your agenda?")
     ent_answer = input("Please type yes or no." )
-
-while ent_answer == "no":
-    entertainment_selected = select_entertainment (random.choice(ent_forms))
-    print("Not what you were thinking?")
-    print("How about a " + entertainment_selected + " instead?")
-    ent_answer = input()
-
-    if ent_answer != "yes" and ent_answer != "no":
+    while ent_answer != "yes" and ent_answer != "no":
         print("You must enter yes or no")
         ent_answer = input()
-    
-    else:
-        entertainment = entertainment_selected
 
-print(place_selectedn, restaurant_selected, transportation_selected, entertainment_selected)
+    while ent_answer == "no":
+        entertainment_selected = select_transportation (random.choice(ent_forms))
+        print("Not what you had in mind?")
+        print("How about a " + entertainment_selected + " ?")
+        ent_answer = input()
+        while ent_answer != "yes" and ent_answer != "no":
+            print("You must enter yes or no")
+            ent_answer = input()
+
+
+print("We are excited to book the following trip for you! Please confirm the following answers are correct")
+print("Your destination is " + place_selected + ".")
+print("Your restaurant selected is " + restaurant_selected + ".")
+print("Your transporation selected is " + transportation_selected + ".")
+print("Your entertainment selected will be a " + entertainment_selected + ".")
+
+#confirm day trip
+print("Does your information look correct? Please type yes or no")
+confirm_trip = input()
+while confirm_trip != "yes" and confirm_trip != "no":
+        print("You must enter yes or no")
+        confirm_trip = input()
+while confirm_trip == "no":
+    print("We are sorry to hear that. Please start the process over.")
+    confirm_trip = input("Please type yes or no." )
+    while confirm_trip != "yes" and confirm_trip != "no":
+        print("You must enter yes or no")
+        confirm_trip = input()
+
+print(f'You will be visiting luxurious {place_selected} and enjoying fine dining at {restaurant_selected}. You will be travelling by {transportation_selected} and enjoying some fantastic entertainment at the local {entertainment_selected}! Enjoy!!')
+
 
 #if I don't like it I can select again
 
